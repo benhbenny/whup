f2cad12e6862b8f46073d49535256aee47da981f
kdiaby <karamokoba.diaby@inria.fr>
2015-08-18 15:41:44 +0200

    second commit (app files)

---
+++ README.md
@@ -0,0 +1,4 @@
+whup
+====
+
+A Symfony project created on August 18, 2015, 2:24 pm.
---
+++ src/WhupBundle/WhupBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WhupBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WhupBundle extends Bundle
+{
+}
